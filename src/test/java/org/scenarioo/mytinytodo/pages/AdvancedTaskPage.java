72c13167406cea647e267400bff85c22e183516a
danielsuter <suter_daniel@hotmail.com>
2014-10-05 17:22:07 -0700

    Added sample webtest stubs

---
+++ src/test/java/org/scenarioo/mytinytodo/pages/AdvancedTaskPage.java
@@ -0,0 +1,12 @@
+package org.scenarioo.mytinytodo.pages;
+
+import org.scenarioo.selenium.infrastructure.pages.WebPage;
+
+public class AdvancedTaskPage extends WebPage{
+
+	public void enter() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
